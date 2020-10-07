7495f2a73d6a86dc2524b338a567bf6092ee8f70
thackett117 <thackett117@gmail.com>
2020-10-07 12:15:16 -0500

    assigning html elements and creating class structures

---
+++ app.js
@@ -0,0 +1,48 @@
+//buttons
+let btnRec = document.getElementById('btnRectangle');
+let btnSqu = document.getElementById('btnSquare');
+let btnCir = document.getElementById('btnCircle');
+let btnTri = document.getElementById('btnTriangle');
+
+//text inputs
+let widthRec = document.getElementById('widthRec');
+let heightRec = document.getElementById('heightRec');
+let lengthSq = document.getElementById('lengthSq');
+let radiusCir = document.getElementById('radiusCir');
+let heightTri = document.getElementById('heightTri');
+
+
+//classes of shapes
+class Shape {
+    constructor() {
+
+    }
+}
+
+
+class Rectangle extends Shape {
+    constructor() {
+
+    }
+}
+
+
+class Square extends Shape {
+    constructor() {
+
+    }
+}
+
+
+class Circle extends Shape {
+    constructor() {
+
+    }
+}
+
+
+class Triangle extends Shape {
+    constructor() {
+        
+    }
+}
